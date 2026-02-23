630b74f196ef326013817f946f373ea8489cc8a4
Vaughncarl <135509932+Vaughncarl@users.noreply.github.com>
2026-02-23 09:39:22 +0300

    Added Stack data structure

---
+++ searching/structures/stack.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    print("Stack:", s.items)
+    print("Pop:", s.pop())
+    print("Peek:", s.peek())
